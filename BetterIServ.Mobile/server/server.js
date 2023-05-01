3678e1008b328543a0b08633474f7a903b1af9aa
leon.hoppe <leon@ladenbau-hoppe.de>
2023-05-01 18:40:19 +0200

    switched to express

---
+++ BetterIServ.Mobile/server/server.js
@@ -0,0 +1,11 @@
+﻿const fileExists = require('fs').existsSync;
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 80;
+
+app.get('**', (req, res) => {
+  let file = req.url;
+  if (!fileExists("./www/" + file)) file = "index.html";
+  res.sendFile(file, { root: "www" });
+});
+app.listen(port, () => console.log("Server started on: " + port));
